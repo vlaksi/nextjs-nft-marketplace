7f83f9575e9bf2625257403d08109aabdede20fa
Vladislav Maksimovic <daksrapsody@gmail.com>
2022-11-03 18:05:42 +0100

    feature: move head to the app instead of the home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,21 @@
 import '../styles/globals.css';
 import { MoralisProvider } from 'react-moralis';
 import Header from '../components/Header';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MoralisProvider initializeOnMount={false}>
-      <Header />
-      <Component {...pageProps} />
-    </MoralisProvider>
+    <div>
+      <Head>
+        <title>NFT Marketplace</title>
+        <meta name="description" content="NFT Marketplace" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MoralisProvider initializeOnMount={false}>
+        <Header />
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </div>
   );
 }
 
