5e75b5a01fb52c5f97fcda24ba352718775c42a0
Vladislav Maksimovic <daksrapsody@gmail.com>
2022-11-09 13:10:05 +0100

    feature: not connect wallet info

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import styles from '../styles/Home.module.css';
 import { useMoralis } from 'react-moralis';
 import NFTBox from '../components/NFTBox';
 import networkMapping from '../constants/networkMapping.json';
 import GET_ACTIVE_ITEMS from '../constants/subgraphQueries';
 import { useQuery } from '@apollo/client';
+import { Card, Illustration } from 'web3uikit';
 
 export default function Home() {
   const { isWeb3Enabled, chainId } = useMoralis();
@@ -13,11 +13,21 @@ export default function Home() {
   const { loading, error, data: listedNfts } = useQuery(GET_ACTIVE_ITEMS);
 
   console.log({ listedNfts });
+  const imageURI =
+    'https://png.pngtree.com/png-vector/20220519/ourmid/pngtree-electric-socket-and-plug-disconnect-png-image_4684939.png';
 
   return (
     <div className="container mx-auto">
-      <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
-      <div className="flex flex-wrap">
+      {isWeb3Enabled && (
+        <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
+      )}
+
+      <div
+        className="flex flex-wrap"
+        style={{
+          justifyContent: 'center',
+        }}
+      >
         {isWeb3Enabled ? (
           loading || !listedNfts ? (
             <div>Loading...</div>
@@ -38,7 +48,24 @@ export default function Home() {
             })
           )
         ) : (
-          <div>Web3 Currently Not Enabled</div>
+          <div
+            style={{
+              width: '500px',
+              marginTop: '10%',
+            }}
+          >
+            <Card
+              description="We do not own private keys and cannot access your funds without your confirmation"
+              onClick={function noRefCheck() {}}
+              setIsSelected={function noRefCheck() {}}
+              title="Connect Your Wallet"
+              tooltipText="Connect with one of available wallet providers or create a new wallet"
+            >
+              <div>
+                <Illustration height="230px" logo="bundle" width="100%" />
+              </div>
+            </Card>
+          </div>
         )}
       </div>
     </div>
