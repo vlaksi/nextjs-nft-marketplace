a215575f4f4e9217ce2d598fc0e0ab75149982b3
Vladislav Maksimovic <daksrapsody@gmail.com>
2022-11-09 11:26:30 +0100

    feature: use apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,13 @@ import '../styles/globals.css';
 import { MoralisProvider } from 'react-moralis';
 import Header from '../components/Header';
 import Head from 'next/head';
+import { NotificationProvider } from 'web3uikit';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: process.env.NEXT_PUBLIC_SUBGRAPH_URL,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,8 +19,12 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MoralisProvider initializeOnMount={false}>
-        <Header />
-        <Component {...pageProps} />
+        <ApolloProvider client={client}>
+          <NotificationProvider>
+            <Header />
+            <Component {...pageProps} />
+          </NotificationProvider>
+        </ApolloProvider>
       </MoralisProvider>
     </div>
   );
